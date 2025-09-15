6bfa460d0028a472f91f360a0f9d8ba408a6e77c
xzakota <xzakota@gmail.com>
2025-09-15 20:53:15 +0800

    Release: 0.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
     val projectGroup by extra("com.xzakota.xposed")
 
     group = projectGroup
-    version = "0.1.3"
+    version = "0.1.4"
 
     plugins.withType(JavaPlugin::class.java) {
         extensions.configure(JavaPluginExtension::class.java) {
